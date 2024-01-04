ed3856ed38aaea9055bd9f90fb7fb4487f3ce00e
Hamideh Parsaeian <hamideh.coding.journey@gmail.com>
2024-01-04 09:33:37 -0500

    Modify style

--- src/App.js
+++ src/App.js
@@ -8,10 +8,56 @@ function App() {
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <Box>
-        <Button onClick={() => dispatch(decrement)}>Decrement</Button>
-        <Typography>{counter}</Typography>
-        <Button onClick={() => dispatch(increment)}>Increment</Button>
+      <Box
+        component="section"
+        sx={{
+          p: 2,
+          border: "1px solid grey",
+          width: 400,
+          height: 100,
+          borderRadius: 2,
+          bgcolor: "text.disabled",
+          display: "flex",
+          alignItems: "center",
+          mx: "auto",
+          my: 16,
+        }}
+      >
+        <Button
+          onClick={() => dispatch(decrement)}
+          variant="contained"
+          color="error"
+          size="large"
+          sx={{
+            fontWeight: 700,
+          }}
+        >
+          Decrement
+        </Button>
+        <Typography
+          variant="button"
+          color="common.white"
+          sx={{
+            display: "inline",
+            px: 2,
+            m: "auto",
+            fontSize: "h4.fontSize",
+            fontWeight: 700,
+          }}
+        >
+          {counter}
+        </Typography>
+        <Button
+          onClick={() => dispatch(increment)}
+          variant="contained"
+          color="success"
+          size="large"
+          sx={{
+            fontWeight: 700,
+          }}
+        >
+          Increment
+        </Button>
       </Box>
     </div>
   );
